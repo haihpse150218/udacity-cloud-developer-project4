4a2a06edb5d8f19fa4d4390f33a7f7a558055d5a
HaiHP <haihpse150218@gmail.com>
2024-09-28 20:28:49 +0700

    Update code FrontEnd

--- starter/client/config.ts
+++ starter/client/config.ts
@@ -1,6 +1,7 @@
 // Auth0 domain
-const REACT_APP_AUTH0_DOMAIN="dev-1vi5xtwstbn62ed5.us.auth0.com"
+const REACT_APP_AUTH0_DOMAIN = 'dev-rr0bgdqh6yltrw73.us.auth0.com'
 // Auth0 client Id
-const REACT_APP_AUTH0_CLIENT_ID="Eli9Jffob9sKRUT8kY5Er66DNO6eA6El"
+const REACT_APP_AUTH0_CLIENT_ID = 'AE1lEkFwiZ5kFk5BA7AKmbhSEGvlZTLz'
 // Serverless api endpoint
-const REACT_APP_API_ENDPOINT="https://5j1mh6pwr2.execute-api.us-east-1.amazonaws.com/dev"
+const REACT_APP_API_ENDPOINT =
+  'https://0x3g44tkb2.execute-api.us-east-1.amazonaws.com/dev/'
